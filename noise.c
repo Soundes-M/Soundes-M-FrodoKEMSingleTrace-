4cb63369fb643528e2c38890dc0e742b60f5c712
soundes-m <marzougui.soundes@gmail.com>
2022-06-26 15:45:28 +0200

    countermeasures deviation tested

--- noise.c
+++ noise.c
@@ -19,28 +19,34 @@ void frodo_sample_n(uint16_t *s, const size_t n)
 { // Fills vector s with n samples from the noise distribution which requires 16 bits to sample. 
   // The distribution is specified by its CDF.
   // Input: pseudo-random values (2*n bytes) passed in s. The input is overwritten by the output.
-    unsigned int i, j;
+    unsigned int i, j, y, Bexp=0;
+    int seed=0;
     printf("Start \n \n");
     for (i = 0; i < n; ++i) {
         uint16_t sample = 0;
         uint16_t prnd = s[i] >> 1;    // Drop the least significant bit
         uint16_t sign = s[i] & 0x1;    // Pick the least significant bit
 
-       // No need to compare with the last value.
-rej:   sample = 0;
+       // No need to compare with the last value. 
        for (j = 0; j < (unsigned int)(CDF_TABLE_LEN_NEW - 1); j++) {
             // Constant time comparison: 1 if CDF_TABLE[j] < s, 0 otherwise. Uses the fact that CDF_TABLE[j] and s fit in 15 bits.
             sample += (uint16_t)(CDF_TABLE_NEW[j] - prnd) >> 15;
         }
         // Assuming that sign is either 0 or 1, flips sample iff sign = 1
-         int y = rand()%3;
-         int Bexp = exp((-y *(y+2*2*sample))/2*2.75*2.75);
-         if (Bexp==0) goto rej;
-         sample = y + 2* sample;
+         srand(seed);
+         while(Bexp==0)
+         {
+            y = rand()%3;
+            Bexp = exp((-y *(y+2*2*sample))/2*2.75*2.75);
+             
+             
+         }
+         Bexp = 0;
+        
+         sample = y+2* sample;
          // assign the sign
          s[i] = ((-sign) ^ sample) + sign; 
-    
-        printf("%ld , ", s[i]);
+     
     }
     
     
